bdfef56349bfddeee7da7336d70323980cc86e58
Diana Nicole <dndanga37@gmail.com>
2024-05-25 17:40:08 +0800

    Updated pass field

---
+++ src/ibm_project/logIn_SignUp.java
@@ -0,0 +1,21 @@
+package ibm_project;
+
+import ibm_project.logIn;
+
+public class logIn_SignUp {
+
+    public static void main(String[] args) {
+        
+        //Instantiation of the logIn Frame
+        logIn logInFrame = new logIn();
+        //Visibility of the frame
+        logInFrame.setVisible(true);
+        //for size policy
+        logInFrame.pack();
+        //centers the placement of the frame when run
+        logInFrame.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
